8528618760c580d0dfe94843a240a9b6621c03ce
Margaret <mgmy08@gmail.com>
2015-03-11 21:03:10 -0500

    Updated index page to include profile pic split and added padding for second full screen row to account for navbar

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -16,31 +16,15 @@
 		}
 	</style>
 @stop
-@section('content')
-		<div id="openingSplash">
-			<p id="splashMsg">"Work hard for what you want because it won't come to you without a fight. You have to be strong and courageous and know that you can do anything you put your mind to. If somebody puts you down or criticizes you, just keep on believing in yourself and turn it into something positive."</p>
-			<span id="quote-author"> -Leah LaBelle </span>
-		</div>
-
-		<div id="welcomeMsg">
-			<p>"Welcome!"</p>
-		</div>
-@stop
 @section('endscript')
     var BV = new $.BigVideo();
 	BV.init();
 	if (Modernizr.touch) {
 		BV.show('/img/home_img.jpg');
 	} else {
-		BV.show('/vids/dock.mp4',{ambient:true});
+		BV.show("/vids/splash.mp4",{ambient:true});
 	}
-});
-$(window).load(function() {
-	$('#openingSplash').addClass('animated pulse').show('slow', function() {
-		    $(this).delay(12000).hide('slow', function() {
-				$('#welcomeMsg').css("font-size","2.25em").addClass('animated fadeInDown').show('slow').delay(9000);
-				window.location.replace('index');
-
-			});
-		});
+	window.setTimeout(function() {
+		window.location.replace('index');
+		}, 17000);
 @stop
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,50 +1,68 @@
 @extends('layouts.master')
 @section('additionalStyle')
 	<style type="text/css">
+		body, .btn-link {
+			background: #FFFEF9 !important;
+			color: #000;
+		}
 		.homeContent {
-			width: 720px;
+			width: 100%;
 			height: 100%;
-			padding: 10px;
-			margin: 0 auto;
-			background-color: rgba(255,252,243, 1);
 			border-radius: 7px;
 		}
-		#profilePic {
+		.container-fluid-custom {
+			padding-left: 0 !important;
+			padding-right: 0 !important;
+		}
+		#profilePic-right {
 			width: 300px;
-			height: 340px;
-			background: url(/img/profile.jpg) no-repeat center center;
+			height: 100%;
+			background: url(/img/head-right.png) no-repeat center center;
 			background-size: cover;
-			float: left;
-			margin-right: 10px;
-			margin-bottom: 10px;
+			display: table-cell;
+		}
+		#profilePic-left {
+			width: 300px;
+			height: 100%;
+			background: url(/img/head-left.png) no-repeat center center;
+			background-size: cover;
+			display: table-cell;
 		}
 		#aboutMe {
-			clear: right;
 			font-size: 1.15em;
+			height: 100vh;
+			width: auto;
+			padding-left: 30px;
+			padding-right: 30px;
+			display: table-cell;
 		}
 		#aboutMe > h1 {
 			text-align: center;
 		}
 		.profileRow {
-			border-bottom: 2px solid black;
+			height: 100vh;
 			margin-bottom: 5px;
+		}
+		.profileRow:nth-of-type(2) {
+			padding-top: 54px !important;
 		}
 	</style>
 @stop
 @section('content')
 	<div class="homeContent">
 		<div class="profileRow">
-			<div id="profilePic"></div>
+			<div id="profilePic-left"></div>
 			<div id="aboutMe">
-			<h1>About Me</h1>
-			<p> My name is Margaret. I was born and raised in a Liberian household in Fort Worth, TX. I then moved to Austin to obtain a Bachelors in Business Administration from the University of Texas. I realized that my heart was not in the area of study I had devoted a bulk of my college career in so I decided to look elsewhere. I then began to revisit my intest in web developement and am currently attending Codeup to obtain the skills I need to make this a career. </p>
-			<p> Even though I have only been on this earth for 25 years or so, I have decided to seek happiness as my ultimate goal. Life is already hard and, in my honest opinion, there is no need to make it worse chasing things that will not make me happy. I am very excited for my life as of now and want to see what this adventure will bring. </p>
+				<h1>About Me</h1>
+				<p> My name is Margaret. I was born and raised in a Liberian household in Fort Worth, TX. I then moved to Austin to obtain a Bachelors in Business Administration from the University of Texas. I realized that my heart was not in the area of study I had devoted a bulk of my college career in so I decided to look elsewhere. I then began to revisit my intest in web developement and am currently attending Codeup to obtain the skills I need to make this a career. </p>
+				<p> Even though I have only been on this earth for 25 years or so, I have decided to seek happiness as my ultimate goal. Life is already hard and, in my honest opinion, there is no need to make it worse chasing things that will not make me happy. I am very excited for my life as of now and want to see what this adventure will bring. </p>
 			</div>
-			</div>
+			<div id="profilePic-right"></div>
+		</div>
 		<div class="profileRow">
 			<h1 id="work">My Work</h1>
 			<p> My name is Margaret. I was born and raised in a Liberian household in Fort Worth, TX. I then moved to Austin to obtain a Bachelors in Business Administration from the University of Texas. I realized that my heart was not in the area of study I had devoted a bulk of my college career in so I decided to look elsewhere. I then began to revisit my intest in web developement and am currently attending Codeup to obtain the skills I need to make this a career. </p>
 		</div>
 	</div>
-
 @stop
+

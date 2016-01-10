e1f3e9d1051b02cdbef2340ffac87fb74c460178
Margaret <mgmy08@gmail.com>
2016-01-10 16:37:41 -0600

    updating due date

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 $(document).ready(function() {
 	"use strict";
 	$("#countdown").countdown({
-		date: "10 july 2016 12:00:00", /** Enter new date here **/
+		date: "12 july 2016 12:00:00", /** Enter new date here **/
 		format: "on"
 	},
 	function() {

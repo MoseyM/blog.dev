b1bee440dcd90181612c62143e490491b349b6a4
MoseyM <mmoseyee@gmail.com>
2016-05-23 00:56:16 -0500

    updated lay for nivanno services

--- public/nirvanno/js/master.js
+++
@@ -1,9 +0,0 @@
-/** Jquery Share Initialize **/
-
-$(document).ready(function() {
-    $('#mydiv').share({
-	    networks: ['email','pinterest','tumblr','googleplus','digg','in1','facebook','twitter','linkedin','stumbleupon'],
-	    theme: 'square'
-    });
-});
-	
